4466de5b1461777e68aec0ac6e8ef7815f3e5189
raf047 <rafhovhannisian@gmail.com>
2020-02-23 20:24:55 +0400

    homework4

---
+++ homework4/src/problem1.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Input seconds: ");
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        if (x * y > 0 || (x == 0 && y == 0))
+            System.out.println(" TRUE ");
+        else
+            System.out.println(" FALSE ");
+    }
+}
